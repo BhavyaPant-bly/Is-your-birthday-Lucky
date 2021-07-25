16ead6e7e332e271c83640b1ed1e1aef6847e0b1
BhavyaPant-bly <51478248+BhavyaPant-bly@users.noreply.github.com>
2021-07-25 21:06:32 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,95 @@
+import "./styles.css";
+import { useState } from "react";
+// import DatePicker from "react-datepicker";
+// import "react-datepicker/dist/react-datepicker.css";
+
+// export default function App() {
+
+// handleDateChange = (e) => {
+//     e.preventDefault();
+//   }
+
+//   return (
+//     <DatePicker
+//     isClearable
+//     showMonthDropdown
+//     showYearDropdown
+//     scrollableMonthYearDropdown
+//     dropdownMode='select'
+//     dateFormat='dd/MM/yyyy'
+//     maxDate={new Date()}
+//     className="edit"
+//     selected={date}
+//     placeholderText="Select Date 'DD/MM/YYYY'"
+//     // readOnly={true}
+//     onChange={date => setDate(date)} />
+//   );
+//  }
+
+export default function App() {
+  const [date, getDate] = useState(new Date());
+  const [luckyno, setnumber] = useState();
+  const [messageid, setmessage] = useState(0);
+  var message = [
+    "",
+    "Yay!! your BirthDate is Lucky",
+    "Sorry!! Your Birth Date is Not Lucky, Please enter another Number or Date"
+  ];
+  var list1;
+  function datehandler(event) {
+    getDate(event.target.value.split("-"));
+  }
+  function LuckyNumber(event) {
+    setnumber(parseInt(event.target.value), 10);
+  }
+
+  function CalculateLuckyDate(event) {
+    event.preventDefault();
+    let sum =
+      parseInt(date[0], 10) + parseInt(date[1], 10) + parseInt(date[2], 10);
+    if (luckyno > 0) {
+      if (sum % luckyno == 0) setmessage(1);
+      else setmessage(2);
+    } else setmessage(0);
+  }
+  return (
+    <div className="App">
+      <h1 className="heading">Is Your Birthday Lucky??</h1>
+
+      <form onSubmit={CalculateLuckyDate}>
+        <input type="date" onChange={datehandler} required />
+        <h1></h1>
+        <input
+          type="number"
+          placeholder="Enter Your Lucky Number"
+          min="1"
+          required
+          onChange={LuckyNumber}
+        />
+        <h1></h1>
+        <input type="submit" value="Check" />
+      </form>
+      <h1>{message[messageid]}</h1>
+      {messageid === 2 ? <img src="/image/sad.svg" alt=":(" /> : null}
+      {messageid === 1 ? <img src="/image/party.svg" alt=":)" /> : null}
+      <div class="footer">
+        @Connect:
+        <a class="link" href="/">
+          instagram
+        </a>
+        <a class="link" href="/">
+          twitter
+        </a>
+        <a class="link" href="/">
+          linkedin
+        </a>
+        <br />
+        <br />
+        <small>
+          Privacy Policy:
+          <br /> We do not store your data. Chillax!!
+        </small>
+      </div>
+    </div>
+  );
+}
